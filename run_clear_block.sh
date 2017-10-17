d31f8ae61bc7d52624813dd20fef42514102e237
kuyseng <kuyseng@gmail.com>
2017-10-17 16:55:01 +0700

    NBC: add run_clear_block

---
+++ run_clear_block.sh
@@ -0,0 +1,5 @@
+#!/usr/local/bin/zsh
+
+rm /Users/kuyseng/Blockchain/iroha/NBC/block_store/*
+rm /Users/kuyseng/Blockchain/iroha/NBC/ABA/block_store/*
+rm /Users/kuyseng/Blockchain/iroha/NBC/ACL/block_store/*
