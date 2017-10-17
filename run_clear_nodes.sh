51a2ca7d43a06d75f726069c6de69e2cd43c4c26
kuyseng <kuyseng@gmail.com>
2017-10-17 16:58:51 +0700

    add run_clear_nodes.sh

---
+++ run_clear_nodes.sh
@@ -0,0 +1,14 @@
+#!/usr/local/bin/zsh
+
+cd /Users/kuyseng/Blockchain/iroha/NBC/
+rm admin* test* genesis.block node*
+
+cd /Users/kuyseng/Blockchain/iroha/NBC/ABA
+rm admin* test* genesis.block node*
+
+cd /Users/kuyseng/Blockchain/iroha/NBC/ACL
+rm admin* test* genesis.block node*
+
+# back to last directory
+cd /Users/kuyseng/Blockchain/iroha/NBC/
+
